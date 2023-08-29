6dd04ce9077ebacd0d1962a7fafec2d6342e0844
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2023-08-29 09:46:29 +0200

    Add layout test (#21)
    * Update Alfa to v0.65.1
    
    * Add test for verifying that layout is scraped
    
    * Use library function to join paths
    
    * Update dependencies
    
    * Undo dependency version updates
    
    In order to fix merge conflict
    
    * Undo yarn.lock update

--- packages/alfa-scraper/test/scraper.spec.ts
+++ packages/alfa-scraper/test/scraper.spec.ts
@@ -1,15 +1,23 @@
+import * as path from "path";
 import * as url from "url";
 
+import { Node, Query } from "@siteimprove/alfa-dom";
 import { test } from "@siteimprove/alfa-test";
 import { Awaiter } from "../src";
 
 import { Scraper } from "../src/scraper";
 
-const fixture = `${url.pathToFileURL(__dirname).href}/fixture`;
+const { getElementDescendants } = Query;
+
+const fixture = path.join(__dirname, "fixture");
+
+function getTestPageFileUrl(fileName: string): string {
+  return url.pathToFileURL(path.join(fixture, fileName)).href;
+}
 
 test("#scrape() scrapes a page with a hash fragment", async (t) =>
   await Scraper.with(async (scraper) => {
-    const url = `${fixture}/internal-link.html`;
+    const url = getTestPageFileUrl("internal-link.html");
     const result = await scraper.scrape(url + "#foo");
 
     t.equal(result.isOk(), true);
@@ -21,7 +29,7 @@ test("#scrape() scrapes a page with a hash fragment", async (t) =>
 
 test("#scrape() scrapes a page with an immediate meta refresh", async (t) =>
   await Scraper.with(async (scraper) => {
-    const url = `${fixture}/meta-refresh-immediate.html`;
+    const url = getTestPageFileUrl("meta-refresh-immediate.html");
     const result = await scraper.scrape(url);
 
     t.equal(result.isOk(), true);
@@ -33,7 +41,7 @@ test("#scrape() scrapes a page with an immediate meta refresh", async (t) =>
 
 test("#scrape() scrapes a page with a delayed meta refresh", async (t) =>
   await Scraper.with(async (scraper) => {
-    const url = `${fixture}/meta-refresh-delayed.html`;
+    const url = getTestPageFileUrl("meta-refresh-delayed.html");
     const result = await scraper.scrape(url);
 
     t.equal(result.isOk(), true);
@@ -45,7 +53,7 @@ test("#scrape() scrapes a page with a delayed meta refresh", async (t) =>
 
 test("#scrape() scrapes a page with an immediate location change", async (t) =>
   await Scraper.with(async (scraper) => {
-    const url = `${fixture}/location-change-immediate.html`;
+    const url = getTestPageFileUrl("location-change-immediate.html");
     const result = await scraper.scrape(url, {
       // Giving a bit of time for the location change to actually happen
       awaiter: Awaiter.duration(100, Awaiter.loaded()),
@@ -60,7 +68,7 @@ test("#scrape() scrapes a page with an immediate location change", async (t) =>
 
 test("#scrape() scrapes a page with a delayed location change", async (t) =>
   await Scraper.with(async (scraper) => {
-    const url = `${fixture}/location-change-delayed.html`;
+    const url = getTestPageFileUrl("location-change-delayed.html");
     const result = await scraper.scrape(url);
 
     t.equal(result.isOk(), true);
@@ -68,4 +76,27 @@ test("#scrape() scrapes a page with a delayed location change", async (t) =>
     const { response } = result.getUnsafe();
 
     t.equal(response.url.toString(), url);
+  }));
+
+test("#scrape() scrapes layout", async (t) =>
+  await Scraper.with(async (scraper) => {
+    const url = getTestPageFileUrl("layout.html");
+    const result = await scraper.scrape(url);
+
+    const box = getElementDescendants(
+      result.getUnsafe().document,
+      Node.fullTree
+    )
+      .find((node) => node.id.some((id) => id === "blackbox"))
+      .getUnsafe()
+      .box.getUnsafe()
+      .toJSON();
+
+    t.deepEqual(box, {
+      type: "rectangle",
+      x: 466,
+      y: 117,
+      width: 271,
+      height: 288,
+    });
   }));
