b067eed4153369042a660c6c1d994e321a1a42cf
Jean-Yves Moyen <jym@siteimprove.com>
2024-06-04 14:20:11 +0200

    Update build scripts

--- scripts/api-extractor.mjs
+++ scripts/api-extractor.mjs
@@ -1,10 +1,9 @@
-const path = require("path");
+import * as path from "path";
 
-const { Extractor, ExtractorConfig } = require("@microsoft/api-extractor");
-
-const { system } = require("./common/system");
-const { flags } = require("./common/flags");
-const { builder } = require("./common/builder");
+import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
+import { system } from "./common/system.mjs";
+import { flags } from "./common/flags.mjs";
+import { builder } from "./common/builder.mjs";
 
 const status = builder.build(flags.project);
 
@@ -15,21 +14,18 @@ if (status !== 0) {
 system.exit(extract(flags.project));
 
 function extract(root = "packages") {
+  const exclude = ["packages", "scratches"];
   const projects = system
     .readDirectory(root, ["tsconfig.json"], ["node_modules"])
-    .map(path.dirname);
+    .map(path.dirname)
+    .filter((x) => !exclude.includes(x));
 
   let code = 0;
 
   for (const project of projects) {
-    let file;
-    try {
-      file = require.resolve(path.resolve(project, "config", "api.json"));
-    } catch {
-      continue;
-    }
+    const filePath = path.resolve(project, "config", "api-extractor.json");
 
-    const config = ExtractorConfig.loadFileAndPrepare(file);
+    const config = ExtractorConfig.loadFileAndPrepare(filePath);
 
     try {
       const result = Extractor.invoke(config, {
