dce1199665e5f8cde53e5127c340f3f093f09b14
Jean-Yves Moyen <jym@siteimprove.com>
2024-07-26 10:51:22 +0200

    Add outcome serialisation test

--- packages/alfa-test-utils/test/sip.spec.tsx
+++ packages/alfa-test-utils/test/sip.spec.tsx
@@ -1,7 +1,9 @@
+import { Diagnostic, Outcome, Rule } from "@siteimprove/alfa-act";
 import { Device } from "@siteimprove/alfa-device";
-import { h } from "@siteimprove/alfa-dom";
+import { Document, Element, h } from "@siteimprove/alfa-dom";
 import { Request, Response } from "@siteimprove/alfa-http";
 import { Serializable } from "@siteimprove/alfa-json";
+import { Err } from "@siteimprove/alfa-result";
 import { alfaVersion } from "@siteimprove/alfa-rules";
 import { test } from "@siteimprove/alfa-test";
 import { Page } from "@siteimprove/alfa-web";
@@ -12,15 +14,15 @@ const { Verbosity } = Serializable;
 
 const device = Device.standard();
 
-test(".payload() creates empty-ish payload", (t) => {
-  const page = Page.of(
-    Request.empty(),
-    Response.empty(),
-    h.document([<span></span>]),
-    Device.standard()
-  );
+function makePage(document: Document): Page {
+  return Page.of(Request.empty(), Response.empty(), document, device);
+}
 
-  const actual = SIP.payload(page, [], "hello", "world");
+test(".payload() creates empty-ish payload", (t) => {
+  const document = h.document([<span></span>]);
+
+  const page = makePage(document);
+  const actual = SIP.payload(page, [], "title", "name");
   actual.RequestTimeStampMilliseconds = 0; // overwriting the unstable one
 
   t.deepEqual(actual, {
@@ -28,7 +30,49 @@ test(".payload() creates empty-ish payload", (t) => {
     Version: alfaVersion,
     CheckResult: "[]",
     Aspects: JSON.stringify(page.toJSON({ verbosity: Verbosity.High })),
-    PageTitle: "hello",
-    TestName: "world",
+    PageTitle: "title",
+    TestName: "name",
+  });
+});
+
+test(".payload serialises outcomes as string", async (t) => {
+  const target = <span>Hello</span>;
+  const document = h.document([target]);
+  const page = makePage(document);
+
+  const rule = Rule.Atomic.of<Page, Element>({
+    uri: "fake rule",
+    evaluate() {
+      return {
+        applicability: () => [target],
+        expectations: (target) => ({
+          1: Err.of(Diagnostic.of("fake diagnostic")),
+        }),
+      };
+    },
+  });
+
+  const outcomes = await rule.evaluate(page);
+
+  const actual = SIP.payload(page, outcomes, "title", "name");
+  actual.RequestTimeStampMilliseconds = 0; // overwriting the unstable one
+
+  const expected: Outcome.Failed.JSON<Element> = {
+    outcome: Outcome.Value.Failed,
+    rule: { type: "atomic", uri: "fake rule", requirements: [], tags: [] },
+    mode: Outcome.Mode.Automatic,
+    target: Serializable.toJSON(target, { verbosity: Verbosity.Minimal }),
+    expectations: [
+      ["1", { type: "err", error: { message: "fake diagnostic" } }],
+    ],
+  };
+
+  t.deepEqual(actual, {
+    RequestTimeStampMilliseconds: 0,
+    Version: alfaVersion,
+    CheckResult: JSON.stringify([expected]),
+    Aspects: JSON.stringify(page.toJSON({ verbosity: Verbosity.High })),
+    PageTitle: "title",
+    TestName: "name",
   });
 });
