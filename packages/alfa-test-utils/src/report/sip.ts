a66266079517b92b6ddaf188ae363cf328a2e279
Jean-Yves Moyen <jym@siteimprove.com>
2024-07-30 16:03:23 +0200

    Alfa test utils (#59)
    * Set up dummy package
    
    * Add basic updload function
    
    * Post-merge clean up
    
    * Add wrapper to upload data to SIP
    
    * Split API key and user name
    
    * Post merge install
    
    * Fix payload
    
    * Add basic test
    
    * Add outcome serialisation test
    
    * Add more config tests
    
    * Accept axios as external dependency
    
    * Update documentation
    
    * Add some upload mocked test
    
    * Stabilise tests
    
    * Update documentation
    
    * Add missing dev dependencies
    
    * Clean up
    
    * Extract API
    
    * Post-merge cleanup
    
    * Use endpoint with pre-signed URLs
    
    * Adapt config tests
    
    * Adapt upload tests
    
    * Update documentation
    
    * Update documentation
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/alfa-test-utils/src/report/sip.ts
@@ -0,0 +1,248 @@
+import { Element, Query } from "@siteimprove/alfa-dom";
+import { Serializable } from "@siteimprove/alfa-json";
+import { alfaVersion } from "@siteimprove/alfa-rules";
+import { Sequence } from "@siteimprove/alfa-sequence";
+import { Page } from "@siteimprove/alfa-web";
+
+import type { AxiosRequestConfig } from "axios";
+import axios from "axios";
+
+import type { alfaOutcome } from "../common.js";
+
+const { Verbosity } = Serializable;
+
+/**
+ * Interacting with Siteimprove Intelligence Platform (SIP) API.
+ *
+ * @public
+ */
+export namespace SIP {
+  /**
+   * @internal
+   */
+  export namespace Defaults {
+    export const URL =
+      "https://api.siteimprove.com/v2/a11y/AlfaDevCheck/CreateReport";
+    export const Title = "Unnamed page";
+    export const Name = "Accessibility Code Checker";
+  }
+
+  /**
+   * Upload the results of an accessibility check to the Siteimprove Intelligence
+   * Platform (SIP) API.
+   *
+   * @public
+   */
+  export async function upload(
+    page: Page,
+    outcomes: Iterable<alfaOutcome>,
+    options: Options
+  ): Promise<string>;
+
+  /**
+   * Internal overload for tests, allowing
+   * * a custom upload URL (use stage / dev URLs); and
+   * * mocking timestamp (timestamp stability in tests).
+   *
+   * @internal
+   */
+  export async function upload(
+    page: Page,
+    outcomes: Iterable<alfaOutcome>,
+    options: Options,
+    override: { url?: string; timestamp?: number }
+  ): Promise<string>;
+
+  export async function upload(
+    page: Page,
+    outcomes: Iterable<alfaOutcome>,
+    options: Options,
+    override: { url?: string; timestamp?: number } = {}
+  ): Promise<string> {
+    const config = Metadata.axiosConfig(page, options, override);
+
+    try {
+      const axiosResponse = await axios.request(config);
+      const { pageReportUrl, preSignedUrl, id } = axiosResponse.data;
+
+      const response = await axios.request(
+        S3.axiosConfig(id, preSignedUrl, page, outcomes)
+      );
+
+      return pageReportUrl;
+    } catch (error) {
+      console.error(error);
+    }
+
+    return "Could not retrieve a page report URL";
+  }
+
+  /**
+   * @public
+   */
+  export interface Options {
+    /**
+     * The username to connect to the Siteimprove Intelligence Platform
+     */
+    userName: string;
+
+    /**
+     * The API key to connect to Siteimprove Intelligence Platform
+     */
+    apiKey: string;
+
+    /**
+     * The title of the page. Defaults to the content of the first `<title>` element,
+     * if any.
+     */
+    pageTitle?: string;
+
+    /**
+     * A unique identifier for the test run, e.g. a git commit hash, branch name, …
+     *
+     * @remarks
+     * Unicity is not required but is recommended to help separating unrelated runs.
+     * Defaults to the generic "Accessibility Code Checker" if none is provided.
+     */
+    testName?: string;
+  }
+
+  /**
+   * Handling the metadata request to the Siteimprove API.
+   *
+   * @internal
+   */
+  export namespace Metadata {
+    interface Payload {
+      RequestTimeStampMilliseconds: number;
+      Version: `${number}.${number}.${number}`;
+      PageTitle: string;
+      TestName: string;
+    }
+
+    /**
+     * Prepare payload with metadata for creating pre-signed URL.
+     */
+    export function payload(
+      PageTitle: string,
+      TestName: string,
+      timestamp: number
+    ): Payload {
+      return {
+        RequestTimeStampMilliseconds: timestamp,
+        Version: alfaVersion,
+        PageTitle,
+        TestName,
+      };
+    }
+
+    /**
+     * Configure parameters of axios request
+     */
+    export function params(url: string, apiKey: string): AxiosRequestConfig {
+      return {
+        method: "post",
+        maxBodyLength: Infinity,
+        url,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + Buffer.from(apiKey).toString("base64"),
+        },
+      };
+    }
+
+    /**
+     * Prepare the configuration for the axios request
+     */
+    export function axiosConfig(
+      page: Page,
+      options: Options,
+      override: { url?: string; timestamp?: number },
+      defaultTitle = Defaults.Title,
+      defaultName = Defaults.Name
+    ): AxiosRequestConfig {
+      const { url = Defaults.URL, timestamp = Date.now() } = override;
+
+      const title =
+        options.pageTitle ??
+        Query.getElementDescendants(page.document)
+          .filter(Element.isElement)
+          .find(Element.hasName("title"))
+          .map((title) => title.textContent())
+          .getOr(defaultTitle);
+
+      const name = options.testName ?? defaultName;
+
+      return {
+        ...params(url, `${options.userName}:${options.apiKey}`),
+        data: JSON.stringify(payload(title, name, timestamp)),
+      };
+    }
+  }
+
+  /**
+   * Handling the data request to the pre-signed S3 URL.
+   *
+   * @internal
+   */
+  export namespace S3 {
+    interface Payload {
+      Id: string;
+      CheckResult: string;
+      Aspects: string;
+    }
+
+    /**
+     * Prepare payload with metadata for creating pre-signed URL.
+     */
+    /**
+     * Prepare payload with Alfa page and results
+     *
+     * @internal
+     */
+    export function payload(
+      Id: string,
+      page: Page,
+      outcomes: Iterable<alfaOutcome>
+    ): Payload {
+      return {
+        Id,
+        CheckResult: JSON.stringify(
+          Sequence.from(outcomes).toJSON({
+            verbosity: Verbosity.Minimal,
+          })
+        ),
+        Aspects: JSON.stringify(page.toJSON({ verbosity: Verbosity.High })),
+      };
+    }
+
+    /**
+     * Configure parameters of axios request
+     */
+    export function params(url: string): AxiosRequestConfig {
+      return {
+        method: "put",
+        maxBodyLength: Infinity,
+        url,
+        headers: { "Content-Type": "application/json" },
+      };
+    }
+
+    /**
+     * Prepare the configuration for the axios request
+     */
+    export function axiosConfig(
+      id: string,
+      url: string,
+      page: Page,
+      outcomes: Iterable<alfaOutcome>
+    ): AxiosRequestConfig {
+      return {
+        ...params(url),
+        data: new Blob([JSON.stringify(payload(id, page, outcomes))], {
+          type: "application/json",
+        }),
+      };
+    }
+  }
+}
---
+++ packages/alfa-test-utils/test/sip.spec.tsx
@@ -0,0 +1,225 @@
+import { Diagnostic, Outcome, Rule } from "@siteimprove/alfa-act";
+import { Device } from "@siteimprove/alfa-device";
+import { Document, Element, h } from "@siteimprove/alfa-dom";
+import { Request, Response } from "@siteimprove/alfa-http";
+import { Serializable } from "@siteimprove/alfa-json";
+import { Err } from "@siteimprove/alfa-result";
+import { alfaVersion } from "@siteimprove/alfa-rules";
+import { test } from "@siteimprove/alfa-test";
+import { Page } from "@siteimprove/alfa-web";
+
+import { SIP } from "../dist/index.js";
+
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+const { Verbosity } = Serializable;
+
+const { Metadata, S3 } = SIP;
+
+const device = Device.standard();
+
+function makePage(document: Document): Page {
+  return Page.of(Request.empty(), Response.empty(), document, device);
+}
+
+test("Metadata.payload() creates a payload", (t) => {
+  const actual = Metadata.payload("title", "name", 0);
+
+  t.deepEqual(actual, {
+    RequestTimeStampMilliseconds: 0,
+    Version: alfaVersion,
+    PageTitle: "title",
+    TestName: "name",
+  });
+});
+test("S3.payload() creates empty-ish payload", (t) => {
+  const document = h.document([<span></span>]);
+
+  const page = makePage(document);
+  const actual = S3.payload("some id", page, []);
+
+  t.deepEqual(actual, {
+    Id: "some id",
+    CheckResult: "[]",
+    Aspects: JSON.stringify(page.toJSON({ verbosity: Verbosity.High })),
+  });
+});
+
+test("S3.payload serialises outcomes as string", async (t) => {
+  const target = <span>Hello</span>;
+  const document = h.document([target]);
+  const page = makePage(document);
+
+  const rule = Rule.Atomic.of<Page, Element>({
+    uri: "fake rule",
+    evaluate() {
+      return {
+        applicability: () => [target],
+        expectations: (target) => ({
+          1: Err.of(Diagnostic.of("fake diagnostic")),
+        }),
+      };
+    },
+  });
+
+  const outcomes = await rule.evaluate(page);
+
+  const actual = S3.payload("some id", page, outcomes);
+
+  const expected: Outcome.Failed.JSON<Element> = {
+    outcome: Outcome.Value.Failed,
+    rule: { type: "atomic", uri: "fake rule", requirements: [], tags: [] },
+    mode: Outcome.Mode.Automatic,
+    target: Serializable.toJSON(target, { verbosity: Verbosity.Minimal }),
+    expectations: [
+      ["1", { type: "err", error: { message: "fake diagnostic" } }],
+    ],
+  };
+
+  t.deepEqual(actual, {
+    Id: "some id",
+    CheckResult: JSON.stringify([expected]),
+    Aspects: JSON.stringify(page.toJSON({ verbosity: Verbosity.High })),
+  });
+});
+
+test("Metadata.params() creates axios config", (t) => {
+  const actual = Metadata.params("url", "key");
+
+  t.deepEqual(actual, {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "url",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + Buffer.from("key").toString("base64"),
+    },
+  });
+});
+test("S3.params() creates axios config", (t) => {
+  const actual = S3.params("url");
+
+  t.deepEqual(actual, {
+    method: "put",
+    maxBodyLength: Infinity,
+    url: "url",
+    headers: { "Content-Type": "application/json" },
+  });
+});
+
+test("Metadata.axiosConfig() creates an axios config", (t) => {
+  const page = makePage(h.document([<span></span>]));
+
+  const actual = Metadata.axiosConfig(
+    page,
+    { userName: "foo@foo.com", apiKey: "bar" },
+    { url: "https://foo.com", timestamp: 0 }
+  );
+
+  t.deepEqual(actual, {
+    ...Metadata.params("https://foo.com", "foo@foo.com:bar"),
+    data: JSON.stringify(
+      Metadata.payload(SIP.Defaults.Title, SIP.Defaults.Name, 0)
+    ),
+  });
+});
+test("S3.axiosConfig() creates an axios config", (t) => {
+  const page = makePage(h.document([<span></span>]));
+
+  const actual = S3.axiosConfig("some id", "a pre-signed S3 URL", page, []);
+
+  t.deepEqual(actual, {
+    ...S3.params("a pre-signed S3 URL"),
+    data: new Blob([JSON.stringify(S3.payload("some id", page, []))], {
+      type: "application/json",
+    }),
+  });
+});
+
+test("Metadata.axiosConfig() uses test name if provided", (t) => {
+  const page = makePage(h.document([<span></span>]));
+
+  const actual = Metadata.axiosConfig(
+    page,
+    { userName: "foo@foo.com", apiKey: "bar", testName: "test name" },
+    { url: "https://foo.com", timestamp: 0 }
+  );
+
+  t.deepEqual(actual, {
+    ...Metadata.params("https://foo.com", "foo@foo.com:bar"),
+    data: JSON.stringify(Metadata.payload(SIP.Defaults.Title, "test name", 0)),
+  });
+});
+
+test("Metadata.axiosConfig() uses explicit title if provided", (t) => {
+  const page = makePage(h.document([<span></span>]));
+
+  const actual = Metadata.axiosConfig(
+    page,
+    { userName: "foo@foo.com", apiKey: "bar", pageTitle: "page title" },
+    { url: "https://foo.com", timestamp: 0 }
+  );
+
+  t.deepEqual(actual, {
+    ...Metadata.params("https://foo.com", "foo@foo.com:bar"),
+    data: JSON.stringify(Metadata.payload("page title", SIP.Defaults.Name, 0)),
+  });
+});
+
+test("Metadata.axiosConfig() uses page's title if it exists", (t) => {
+  const page = makePage(h.document([<title>Hello</title>, <span></span>]));
+
+  const actual = Metadata.axiosConfig(
+    page,
+    { userName: "foo@foo.com", apiKey: "bar" },
+    { url: "https://foo.com", timestamp: 0 }
+  );
+
+  t.deepEqual(actual, {
+    ...Metadata.params("https://foo.com", "foo@foo.com:bar"),
+    data: JSON.stringify(Metadata.payload("Hello", SIP.Defaults.Name, 0)),
+  });
+});
+
+test("Metadata.axiosConfig() uses explicit title over page's title", (t) => {
+  const page = makePage(h.document([<title>ignored</title>, <span></span>]));
+
+  const actual = Metadata.axiosConfig(
+    page,
+    { userName: "foo@foo.com", apiKey: "bar", pageTitle: "page title" },
+    { url: "https://foo.com", timestamp: 0 }
+  );
+
+  t.deepEqual(actual, {
+    ...Metadata.params("https://foo.com", "foo@foo.com:bar"),
+    data: JSON.stringify(Metadata.payload("page title", SIP.Defaults.Name, 0)),
+  });
+});
+
+// Somehow, importing axios-mock-adapter breaks typing.
+// Requiring it is fine, but not allowed in an ESM file.
+// @ts-ignore
+const mock = new MockAdapter(axios);
+
+// Everything will be mocked after that, use mock.restore() if needed.
+mock
+  .onPost(SIP.Defaults.URL)
+  .reply(200, {
+    pageReportUrl: "a page report URL",
+    preSignedUrl: "a S3 URL",
+    id: "hello",
+  });
+
+mock.onPut("a S3 URL").reply(200);
+
+test(".upload connects to Siteimprove Intelligence Platform", async (t) => {
+  const page = makePage(h.document([<span></span>]));
+
+  const actual = await SIP.upload(page, [], {
+    userName: "foo@foo.com",
+    apiKey: "bar",
+  });
+
+  t.deepEqual(actual, "a page report URL");
+});
