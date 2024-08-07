47280b18cc95c3db75dd596cd565cd5fcedbf08d
Jean-Yves Moyen <jym@siteimprove.com>
2024-08-07 11:38:48 +0200

    Remove pre-built rule sets (#63)
    * Remove pre-built rule sets
    
    * Extract API
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/alfa-test-utils/src/audit/rules.ts
+++ packages/alfa-test-utils/src/audit/rules.ts
@@ -3,7 +3,6 @@ import type { Predicate } from "@siteimprove/alfa-predicate";
 import { Refinement } from "@siteimprove/alfa-refinement";
 import rules, { Scope } from "@siteimprove/alfa-rules";
 import type { Flattened } from "@siteimprove/alfa-rules";
-import type { Sequence } from "@siteimprove/alfa-sequence";
 import { Conformance, Criterion } from "@siteimprove/alfa-wcag";
 
 const { and } = Refinement;
@@ -31,13 +30,6 @@ export namespace Rules {
    */
   export const aaFilter: Predicate<Flattened.Rule> = (rule) =>
     rule.hasRequirement(and(Criterion.isCriterion, Conformance.isAA()));
-  /**
-   * The AA conformance rules, i.e. rules for level A and AA success criteria.
-   *
-   * @remarks
-   * This considers rules for the latest recommendation, i.e. WCAG 2.2
-   */
-  export const aaRules = allRules.filter(aaFilter);
 
   /**
    * Filter matching the WCAG 2.0 rules.
@@ -48,30 +40,18 @@ export namespace Rules {
         Iterable.some(criterion.versions, (version) => version === "2.0")
       )
     );
-  /**
-   * The WCAG 2.0 rules.
-   */
-  export const wcag20Rules = allRules.filter(wcag20Filter);
 
   /**
    * Filter matching the WCAG 2.0 AA conformance rules.
    */
   export const wcag20aaFilter: Predicate<Flattened.Rule> = (rule) =>
     rule.hasRequirement(and(Criterion.isCriterion, Conformance.isAA("2.0")));
-  /**
-   * The WCAG 2.0 AA conformance rules.
-   */
-  export const wcag20aaRules = allRules.filter(wcag20aaFilter);
 
   /**
    * Filter matching the WCAG 2.1 AA conformance rules.
    */
   export const wcag21aaFilter: Predicate<Flattened.Rule> = (rule) =>
     rule.hasRequirement(and(Criterion.isCriterion, Conformance.isAA("2.1")));
-  /**
-   * The WCAG 2.1 AA conformance rules.
-   */
-  export const wcag21aaRules = allRules.filter(wcag21aaFilter);
 
   /**
    * Filter matching the "component" rules.
@@ -83,15 +63,6 @@ export namespace Rules {
    */
   export const componentFilter: Predicate<Flattened.Rule> = (rule) =>
     rule.hasTag((tag) => tag.equals(Scope.Component));
-  /**
-   * The "component" rules.
-   *
-   * @remarks
-   * This discards rules that only make sense in the context of a full page, e.g.
-   * skip link or presence of a title. It can be used to test design system pages
-   * that contain a single component to showcase it.
-   */
-  export const componentRules = allRules.filter(componentFilter);
 
   /**
    * Filter to manually cherry-pick rules, by their id.
@@ -126,30 +97,5 @@ export namespace Rules {
     );
 
     return (rule) => rulesURIs.includes(rule.uri);
-  }
-
-  /**
-   * Helper to build a list of cherry-picked rules.
-   */
-  export function cherryPickRules(
-    rulesId: Array<number>
-  ): Sequence<Flattened.Rule>;
-
-  /**
-   * Helper to build a list of cherry-picked rules.
-   */
-  export function cherryPickRules(
-    ...rulesId: Array<number>
-  ): Sequence<Flattened.Rule>;
-
-  export function cherryPickRules(
-    first: number | Array<number>,
-    ...rulesId: Array<number>
-  ) {
-    return allRules.filter(
-      typeof first === "number"
-        ? cherryPickFilter(first, ...rulesId)
-        : cherryPickFilter(first)
-    );
   }
 }
