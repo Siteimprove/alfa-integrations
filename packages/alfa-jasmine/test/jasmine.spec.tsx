f26b526b7d660afb4724a774192a3e5043d472bd
Jean-Yves Moyen <jym@siteimprove.com>
2025-12-05 14:25:11 +0100

    Add basic jasmine setup

---
+++ packages/alfa-jasmine/test/jasmine.spec.tsx
@@ -0,0 +1,7 @@
+/// <reference types="jasmine" />
+
+describe("jasmine", () => {
+  it("should ", () => {
+    expect(1 + 1).toBe(3);
+  });
+});
--- scripts/test.mjs
+++ scripts/test.mjs
@@ -23,16 +23,13 @@ function test(root = "packages") {
     system.readDirectory(root, [".spec.ts", ".spec.tsx"], ["node_modules"]),
     os.cpus().length,
     (fileName, done) => {
-      // These tests run with their own test framework and must therefore **not**
+      // These tests run with their own test launcher and must therefore **not**
       // be run through this script!
-      const testFrameworks = ["cypress", "jest", "vitest"];
+      const testFrameworks = ["cypress", "jasmine", "jest", "vitest"];
 
       if (testFrameworks.some((framework) => fileName.includes(framework))) {
-        // Cypress is a special kid including its own test framework, so we
-        // cannot easily rely on alfa-test and skip it here. It needs to be
-        // tested separately
         system.write(
-          `Skipping tests having their own framework (${fileName}), use \`yarn test:other\` instead`
+          `Skipping tests having their own framework (${fileName}), use \`yarn test:other\` instead`,
         );
 
         return;
@@ -53,13 +50,13 @@ function test(root = "packages") {
         stdio: "inherit",
       }).then(
         () => done(),
-        (err) => done(err)
+        (err) => done(err),
       );
     },
     (err) => {
       if (err) {
         system.exit(1);
       }
-    }
+    },
   );
 }
