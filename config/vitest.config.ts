2ef084fece29154782650f41edf5994e4c49377a
Jean-Yves Moyen <jym@siteimprove.com>
2025-12-08 11:42:39 +0100

    Enable alfa-test on alfa-vitest

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["packages/alfa-*/test/**/*.spec.ts?(x)"],
+    // Packages with integration to a tests launcher use that launcher, and are
+    // therefore skipped here. Use `yarn test:other` to run those.
     exclude: [
+      "packages/alfa-cypress",
+      "packages/alfa-jest",
+      // TODO
       "packages/alfa-angular",
       "packages/alfa-assert",
       "packages/alfa-chai",
@@ -11,7 +16,6 @@ export default defineConfig({
       "packages/alfa-cli",
       "packages/alfa-command",
       "packages/alfa-crawler",
-      "packages/alfa-cypress",
       "packages/alfa-enzyme",
       "packages/alfa-formatter",
       "packages/alfa-formatter-earl",
@@ -20,7 +24,6 @@ export default defineConfig({
       "packages/alfa-frontier",
       "packages/alfa-interviewer",
       "packages/alfa-jasmine",
-      "packages/alfa-jest",
       "packages/alfa-jquery",
       "packages/alfa-playwright",
       "packages/alfa-puppeteer",
@@ -28,7 +31,6 @@ export default defineConfig({
       "packages/alfa-scraper",
       "packages/alfa-selenium",
       "packages/alfa-unexpected",
-      "packages/alfa-vitest",
       "packages/alfa-vue",
       "packages/alfa-webdriver",
     ],
